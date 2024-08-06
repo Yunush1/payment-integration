8a3a84a181e37fb7b961f5bf74e343f4cfb25fbb
Yunush1 <105458936+Yunush1@users.noreply.github.com>
2024-08-06 21:39:39 +0530

    Blog controller improvements

--- payment_service/blog.controller.js
+++ payment_service/blog.controller.js
@@ -5,13 +5,13 @@ exports.getBlogs = async(req, res)=>{
     try{
         
         const client= createClient()
-        const bl =await client.get('blog');
-        if(bl){
-            res.status(200).json(bl);
-        }
+        // const bl =await client.get('blog');
+        // if(bl){
+        //     res.status(200).json(bl);
+        // }
         const blog = await Blog.find();
-        client.set('blog',blog)
-        console.log(await client.get('blog'));
+        // client.set('blog',blog)
+        // console.log(await client.get('blog'));
        return res.status(200).json(blog);
     }catch(err){
        return res.status(400).json({
